54a68fc73ae30927858f1795b8eb63a2324dee15
Archil Sikharulidze <softdevelopment@yahoo.com>
2026-07-16 23:12:10 +0400

    Additiona

---
+++ src/homework_9/Figures/Circle.java
@@ -0,0 +1,20 @@
+package homework_9.Figures;
+
+public class Circle extends Figure {
+    public final double P = 3.14;
+    public double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return P * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * P * radius;
+    }
+}
---
+++ src/homework_9/Figures/Figure.java
@@ -0,0 +1,6 @@
+package homework_9.Figures;
+
+public abstract class Figure {
+    public abstract double getArea();
+    public abstract double getPerimeter();
+}
